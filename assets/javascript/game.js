69b876c4a711c926d8ea857496ca489a8ab36cbd
Jeremiah Stafford <jstafford@slmdevice.com>
2019-08-31 17:19:05 -0400

    game completed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,80 +3,71 @@ window.onload = function () {
     $("#gettar").on("click", targrange);
 
 };
+//Global Variables
 var targmin = 19;
 var targmax = 120;
-// var target =  targrange (targmin,targmax);
 var gemmin = 1;
 var gemmax = 12;
+var counter = 0;
+var wins = 0;
+var losses = 0;
+var games = 0;
+var gemOpts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];              //Array of possibel numbers
+var gemSel = [];                                                    //Array of randomly selected numbers assigned to gems
+var target = Math.floor(Math.random() * (targmax - targmin + 1) + targmin); 
 
-var gem2 = 0;
-var gem3 = 0;
-var gem4 = 0;
 
 // Function to create random target number between 19 & 120
-
 function targrange(min, max) {
-    var target = Math.floor(Math.random() * (targmax - targmin + 1) + targmin);
-    $('#target').html('<h2>' + target + '<h2>');
+    $('.target').html('<span>  ' + target + '  <span>');
 }
 
-//Function to create random guess numbers between 1 & 12
-var gemOpts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-var gemSel = [];
-for (i = 1; i < 5; i++) {
+//Recursive function to create random guess numbers between 1 & 12
+for (i = 0; i < 4; i++) {
     var seed = Math.floor(Math.random() * gemOpts.length + 1);
-    gemSel.push(seed);
-    gemOpts = jQuery.grep(gemOpts, function (n) {
+                                                                    //seed variable randomly selected as length of remaining array changes
+    gemSel.push(seed);                                              //seeds are pushed to gemSel array
+    gemOpts = jQuery.grep(gemOpts, function (n) {                   //gemOpts array is adjusted for each removal of the seed
         return (n !== seed);
-    });
+    });    
+    var gemImage = $('#gem' + parseInt(i + 1));
+    gemImage.addClass("gemImage");  
+    gemImage.attr("data-gemValue", gemSel[i]);
+    $("#gem" + parseInt(i + 1)).append(gemImage);    
 
-    console.log(seed);
-    console.log(gemSel);
-    console.log(gemOpts);
+    var gem1 = gemSel[0];
+    var gem2 = gemSel[1];
+    var gem3 = gemSel[2];
+    var gem4 = gemSel[3];
 }
+    console.log(gem1);
+    console.log(gem2);
+    console.log(gem3);
+    console.log(gem4);
 
-// var seed = Math.floor(Math.random() * 13);
-// gemSel.push(seed);
+    //Assign gem values to specific buttons
+    $(".gemImage").on("click", function() {
+        var gemValue = ($(this).attr("data-gemValue"));   // Determining the crystal's value requires us to extract the value from the data attribute.
+        gemValue = parseInt(gemValue);                    // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
+        counter += gemValue;  
+        $(".score").html('<span>  ' + counter + '  <span>');                                // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
+                                                                  // All of the same game win-lose logic applies. So the rest remains unchanged.
+      if (counter === target) {
+      
+      counter = 0;
+      $('.target').html('<span> 0 <span>');
+      $('.score').html('<span> 0 <span>');
+      $('.wins').html(++wins);
+      $('.games').html(++games);
+    }
 
-// $(gemOpts).each(function (index, value) {
-//     if (value = seed) {
-//         gemOpts = jQuery.grep(gemOpts, function(value) {
-//             return value != seed;
-//         });
-//     }
-//     console.log(gemOpts);
-//     console.log(gemSel);
-// });
+    else if (counter >= target) {
+      
+      counter = 0;
+      $('.target').html('<span> 0 <span>');
+      $('.score').html('<span> 0 <span>');
+      $('.losses').html(++losses);
+      $('.games').html(++games);
+    }
 
-// function getSeeds(){
-//     var gemSel = [];
-//     var seed1 = Math.floor(Math.random() * 13);
-//     gemSel = gemSel.push(seed1);  
-//     var seed2 = Math.floor(Math.random() * 12);
-//     gemSel = gemSel.push(seed2);
-//     var seed3 = Math.floor(Math.random() * 11);
-//     gemSel = gemSel.push(seed3);
-//     var seed4 = Math.floor(Math.random() * 10);
-//     gemSel = gemSel.push(seed4);
-
-
-//     console.log(gemSel);
-
-
-// }
-
-
-
-//     arr = jQuery.grep(arr, function (n, i) {
-    //     return (n !== gem1);
-    // });
-    // console.log(gem1);
-    // console.log(gem2);
-
-    // arr = jQuery.grep(arr, function (a) {
-    //     return a !== 9;
-    // });
-
-    // $("span").text(arr.join(", "));
-
-
+  });
